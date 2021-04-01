0318201a4034becdae87aab00acd84261a44c2e9
lennard.boediger <lennard@nitrokey.com>
2021-04-01 12:09:45 +0200

    fixed version with cat file

--- fido2/version.mk
+++ fido2/version.mk
@@ -1,5 +1,5 @@
 
-SOLO_VERSION_FULL?=$(shell cat STABLE_VERSION)
+SOLO_VERSION_FULL?= 2.4.0.nitrokey #$(shell cat STABLE_VERSION)
 SOLO_VERSION:=$(shell python3 -c 'print("$(SOLO_VERSION_FULL)".split("-")[0])')
 SOLO_VERSION_MAJ:=$(shell python3 -c 'print("$(SOLO_VERSION)".split(".")[0])')
 SOLO_VERSION_MIN:=$(shell python3 -c 'print("$(SOLO_VERSION)".split(".")[1])')
