609e8d4a8ef57414f4d0d10c1b229f94d9ccc8db
lennard.boediger <lennard@nitrokey.com>
2021-04-07 21:06:36 +0200

    release.sh

---
+++ release.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+repo=LennardBoediger/runner-release
+
+version=$(git describe)
+version="${version%-*-*}"
+a="${version%.*.*}" 
+b="${version#*.*.}"
+a=$(echo $a | awk -F. -v OFS=. 'NF==1{print ++$NF}; NF>1{$NF=sprintf("%0*d", length($NF), ($NF+1)); print}')
+version=$a.$b
+
+upload_url=$(curl -s -H "Authorization: token $GITHUB_TOKEN" -d '{"tag_name": "$version", "name":"$version","body":"this is a test release"}' "https://api.github.com/repos/$repo/releases" | jq -r '.upload_url')
+
+upload_url="${upload_url%\{*}"
+
+zip -r builds/release.zip builds/release
+zip -r builds/debug.zip builds/debug
+
+echo "uploading asset to release to url : $upload_url"
+
+curl -s -H "Authorization: token $GITHUB_TOKEN"  \
+        -H "Content-Type: application/zip" \
+        --data-binary @builds/release.zip  \
+        "$upload_url?name=release.zip"
+
+curl -s -H "Authorization: token $GITHUB_TOKEN"  \
+        -H "Content-Type: application/zip" \
+        --data-binary @builds/debug.zip  \
+        "$upload_url?name=debug.zip"
